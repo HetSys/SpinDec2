405e92ffdca03d7b441cc35bf5318d91515496f7
gosling123 <78127892+gosling123@users.noreply.github.com>
2022-04-29 00:24:58 +0100

    added comments
    added comments

--- src/Visualise/plotter.py
+++ src/Visualise/plotter.py
@@ -6,6 +6,9 @@ from scipy.ndimage import uniform_filter1d
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from matplotlib import cm
 
+
+# Pre-set plotting style 
+
 plt.rcParams["figure.figsize"] = [8, 8]
 plt.rcParams["figure.autolayout"] = True
 plt.rcParams['lines.linewidth'] = 2
@@ -16,46 +19,57 @@ plt.rcParams['ytick.labelsize'] = 14
 plt.rcParams['legend.fontsize'] = 14
 
 
+# simple moving average using scipy uniform_filter1d pacakge
+# smooths out free energy result
 def moving_av(Q, span, period = 10):
     return uniform_filter1d(Q, size = span // period)
 
+#calcualtes the value of f(c = c_av) (average bulk free energy)
 def bulk_energy_av(coeffs, c_av):
     bulk = 0
     for i in range(len(coeffs)):
         bulk += coeffs[i] * c_av**i
     return bulk
 
+#Visualisation Code Begins
 class Vis_CH:
 
     """Class implementing Visualisation of Cahn-Hilliard"""
 
+    #initilisation of the class
     def __init__(self, file, av_period):
 
         self.file = file
         self.av_period = av_period
 
-
+    #read in data from output file (netcdf)
     def read_netcdf(self, verbose = True):
 
         data = NC.Dataset(self.file, "r", format="NETCDF4")
     
+        #concentration order parameter c(t, y, x)
         self.c = [data.variables['c'][:]][0]
 
+        #free energy over time
         self.F_tot = [data.variables['F_tot'][:]][0]
 
+        #user inputted coefficients for f(c)
         self.coeffs = [data.variables['coeffs'][:]][0]
 
-        self.dt = data.dt
-        self.N_t = int(data.N_t)
-        self.N_x = int(data.N_x)
-        self.N_y = int(data.N_y)
-        self.M_A = data.M_A
-        self.M_B = data.M_B
-        self.c_0 = data.c_0
-        self.kappa = data.kappa
+        
+        self.dt = data.dt         #time-step
+        self.N_t = int(data.N_t)  #time iterations
+        self.N_x = int(data.N_x)  #x dicretizations
+        self.N_y = int(data.N_y)  #y discretizations
+        self.M_A = data.M_A       #atomic mobility (species A)
+        self.M_B = data.M_B       #atomic mobility (species B)
+        self.c_0 = data.c_0       #user set initail grid average c
+        self.kappa = data.kappa   #gradient term coefficient
 
+        #time array
         self.time = np.arange(self.N_t) * self.dt
 
+        #moving average of F
         if (self.av_period > self.N_t):
             print("WARNING:")
             print('set average period is greater than total time period (N_t = ' + str(self.N_t) + ' setting to default value of 10')
@@ -63,13 +77,14 @@ class Vis_CH:
 
         self.F_av = moving_av(self.F_tot, self.N_t, self.av_period)
 
-        ### frame speed of animation depending on number of files (i.e more files, want to be quicker)
+        #frame speed of animation depending on number of files (i.e more files, want to be quicker)
 
         if (self.N_t >= 1000):
             self.frame_step = 1
         else:
             self.frame_step = 5
 
+        #print metadata to terminal
         if (verbose):
 
             print('\u0394t =', self.dt, 's')
@@ -92,6 +107,7 @@ class Vis_CH:
 
             print(string)
 
+    #function that plots an animation of both c and F (and its moving average) on the same figure
     def dual_animation(self):
         
         fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15,15))
@@ -144,7 +160,8 @@ class Vis_CH:
                         wspace=0.5, 
                         hspace=0.4)
         plt.show()
-
+ 
+    #function that plots an animation of c
     def grid_animation(self):
         
         fig, ax = plt.subplots()
@@ -169,6 +186,7 @@ class Vis_CH:
         anim_grid = animation.FuncAnimation(fig = fig, func = animate_grid, frames=len(self.time), repeat=False, interval=self.frame_step)
         plt.show()
 
+    #function that plots an animation of F (and its moving average)
     def F_animation(self):
         fig, ax = plt.subplots()
 
@@ -195,7 +213,7 @@ class Vis_CH:
         anim_line = animation.FuncAnimation(fig = fig, func = animate_line, frames = len(self.time) - 1, repeat = False, interval=self.frame_step)
         plt.show()
 
-
+    #Function that plots F (and its moving average)
     def F_plot(self):
         fig, ax = plt.subplots()
         ax.plot(self.time,self.F_tot, scaley=True, scalex=True, color="blue", label = r'CH Data')
@@ -205,6 +223,8 @@ class Vis_CH:
         ax.legend(loc = 'upper left')
         plt.show()
 
+    #function thats plots the initial bulk free energy and the trajectory of the 
+    #averge bulk free energy over time in c c-f(c) space
     def bulk_energy_traj(self):
 
             self.c_av = np.array([])
