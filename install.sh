12ac61cf37457a536fce1b40a8f05007c657fa28
Dylan <Dylan.Morgan@warwick.ac.uk>
2022-05-03 00:39:04 +0100

    Debugging and wrote help message #6 Also added linter config file, added all f90 files to txt file, started listing code dependencies, and added .gitignore

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ set -e
 ### Compilation ###
 compile () {
     # Option for choosing the compiler
-    if [[ $1 == "d" ]] || [[ $1 == "debug" ]]; then
+    if [[ $1 == "t" ]] || [[ $1 == "true" ]]; then
         comp_line="gfortran -g -std=f2008 -Wall -fimplicit-none -fcheck=all -Wextra -pedantic -fbacktrace"
-    elif [[ -z $1 ]]; then
+    elif [[ -z $1 ]] || [[ $1 == "f" ]] || [[ $1 == "false" ]]; then
         comp_line="gfortran -g"
     else
         echo -e "Not a valid option for -c/--compile\n"
@@ -20,12 +20,7 @@ compile () {
 
     # Add program files from prog_files.txt
     input_file="./prog_files.txt"
-    prog_files=()
-    while read -r line; do
-        prog_files+=("$line")
-    done < "$input_file"
-
-    echo "prog file: $prog_files"
+    readarray -t prog_files < "$input_file"
 
     # Binary name and location
     compd_file="./bin/spindec"
@@ -37,34 +32,33 @@ compile () {
     echo -e "Compile line: $comp_line\n"
 
     # Compile
-    $comp_line $fflags $prog_files $flibs -o $compd_file
+    # $comp_line $fflags $prog_files $flibs -o $compd_file
 
     # Add binary to $PATH (with some checks)
     while true; do
-        if [[ $debug == true ]]; then
-            read -p -n 1 -r 'Add to $PATH? [Y/n] ' confirmation
+        read -p 'Add binary to $PATH? [Y/n] ' confirmation
 
-            if [[ $confirmation =~ ^[Yy]$ ]] || [[ $confirmation == '' ]]; then
-                if grep -Fq "spindec" $HOME/.bashrc; then
-                    echo 'The binary is already in your $PATH'
-                    break
-                else
-                    if [[ "$SHELL" == *"bash"* ]]; then
-                        working_dir=`pwd`
-                        echo "export PATH=`pwd`/bin/spindec:$PATH" >> $HOME/.bashrc
-                        break
-                    else
-                        echo 'Unable to add to $PATH as bash is not your default shell'
-                        break
-                    fi
-                fi
-
-            elif [[ $confirmation =~ ^[Nn]$ ]]; then
-                echo 'Not added to $PATH'
+        if [[ $confirmation =~ ^[Yy]$ ]] || [[ $confirmation == '' ]]; then
+            if grep -Fq "spindec" $HOME/.bashrc; then
+                echo 'The binary is already in your $PATH'
                 break
             else
-                echo 'Not a valid option'
+                if [[ "$SHELL" == *"bash"* ]]; then
+                    working_dir=`pwd`
+                    echo "export PATH=`pwd`/bin/spindec:$PATH" >> $HOME/.bashrc
+                    echo 'Binary added to $PATH'
+                    break
+                else
+                    echo 'Unable to add to $PATH as bash is not your default shell'
+                    break
+                fi
             fi
+
+        elif [[ $confirmation =~ ^[Nn]$ ]]; then
+            echo 'Not added to $PATH'
+            break
+        else
+            echo 'Not a valid option'
         fi
     done
 }
@@ -92,21 +86,42 @@ ascii_art () {
     echo -E '          %% |'
     echo -E '          \__|'
     echo
+    echo -e ' Modelling the Phase Field of Spinoidal Decomposition\n'
 }
 
 help_message () {
-    echo "Haha good luck"
+    echo "usage: spindec [-h]"
+    echo "               [-c DEBUG]"
+    echo "               [-t]"
+    echo "               [-e]"
+    echo
+    echo "options:"
+    echo "  -h, --help              show this help message and exit"
+    echo "  -c, --compile DEBUG     compile the code with optional debug option"
+    echo "                          (default=false)"
+    echo "  -t, --test              run automated unit tests"
+    echo "  -e, --example           run with example initialisation states"
 }
 
 ### Argument Parser ###
 # Requires util-linux
+# getopt options list
 options=$(getopt -o c::te::h -l compile::,test,example::,help -- "$@")
+
+# Exit if error code
 if [[ $? -ne 0 ]]; then
     exit 1;
 fi
 
+# Help and exit if no options given
+if [[ $# -lt 1 ]]; then
+    ascii_art
+    help_message
+fi
+
 eval set -- "$options"
 
+# Commands associated with options
 while [[ $# -gt 0 ]]; do
     case "$1" in
         -c | --compile)
