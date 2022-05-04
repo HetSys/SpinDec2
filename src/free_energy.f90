d05d4454a050fdbce3ff898435b4d8f9e0dbb335
gosling123 <b.gosling@warwick.ac.uk>
2022-05-04 08:07:28 +0100

    Added PBC's #1

--- src/free_energy.f90
+++ src/free_energy.f90
@@ -11,7 +11,7 @@ module free_energy
     !!
     !! subroutine to calculate Bulk Free Energy at time t
     !!
-    !!@param  f_b : bulk free energy array at time t
+    !!@param f_b: bulk free energy array at time t
     !!@param c: 2D concentration grid - c(x,y)
     !!@param a: 1D array storing coefficients provided by user  
     !!@param kappa : gradient term coefficient
@@ -48,7 +48,7 @@ module free_energy
     !!
     !! subroutine to calculate Total Free Energy at time t
     !!
-    !!@param  F : total free energy at time t
+    !!@param F: total free energy at time t
     !!@param c: 2D concentration grid - c(x,y)
     !!@param dx,dy: spatial step sizes in x and y
     !!@param kappa : gradient term coefficient
@@ -56,8 +56,8 @@ module free_energy
       
     subroutine total_free_energy(F, c, f_b, dx, dy, kappa)
 
-        real(real64), intent(in) :: c(:,:)
-        real(real64) :: f_b(:,:)
+        real(real64), intent(in) :: c(0:,0:)
+        real(real64) :: f_b(0:,0:)
         integer :: Nx, Ny
         integer :: i,j
         real(real64), intent(out) :: F
@@ -72,14 +72,14 @@ module free_energy
         F = 0.0
     
         !Loop filling the mu array
-            do j=1,ny
-                do i=1,nx
+        do j=0,ny-1
+             do i=0,nx-1
 
-                    xlap = (c(i+1,j) - 2*c(i,j) + c(i-1,j)) / (dx*dx)
-                    ylap = (c(i,j+1) - 2*c(i,j) + c(i,j-1)) / (dy*dy)
-                    P = xlap + ylap
+                xlap = (c(modulo(i+1,nx),j)-2*c(i,j)+c(modulo(i-1,nx),j))/(dx*dx)
+                ylap = (c(i,modulo(j+1,ny))-2*c(i,j)+c(i,modulo(j-1,ny)))/(dy*dy)
+                P = xlap + ylap
                     
-                    F = F + (f_b(i,j) + 0.5*kappa*P*P)*dx*dy
+                F = F + (f_b(i,j) + 0.5*kappa*P*P)*dx*dy
                     
                 end do
             end do
