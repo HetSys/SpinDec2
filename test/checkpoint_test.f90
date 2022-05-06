158367789dc7d96a695124f48b552908c5527c31
ShufflerBardOnTheEdge <matyas.parrag@gmail.com>
2022-05-06 14:19:49 +0100

    Added tests for input and checkpinting. Added test.f90 to run tests #17

---
+++ test/checkpoint_test.f90
@@ -0,0 +1,86 @@
+module checkpoint_tester
+    use input_params
+    use checkpointing
+    use netcdf
+    use iso_fortran_env
+
+    implicit none
+
+contains
+
+    subroutine checkpoint_test()
+
+        integer  :: nx,ny,cint,random_seed,err,i,count,ncerr,use_input,current_time
+        character(len=128) :: cpi,cpo
+        real(kind=REAL64) :: initial_conc, conc_std,m1,m2,k,bfe,t,delta_t,df_tol
+        real(kind=real64),dimension(:),allocatable :: coeffs
+        real(kind=real64), dimension(:,:,:), allocatable :: datas
+        real(kind=real64), dimension(:,:), allocatable :: mu
+        real(kind=real64), dimension(:), allocatable :: ftot
+
+        TYPE(run_data_type) :: run_data
+
+        allocate(datas(1,1,1))
+        allocate(mu(1,1))
+        allocate(ftot(1))
+
+
+        datas(1,1,1) = 1
+        mu(1,1) = 1
+        ftot(1) = 1
+        current_time = 0
+        count = 0
+
+        open (unit = 13, file = "Out.cp")
+        close(13)
+
+
+        call read_params("checkpoint_test.txt",initial_conc,conc_std,coeffs,nx,&
+            ny,m1,m2,k,bfe,cint,cpi,cpo,t,delta_t,df_tol,random_seed,use_input,err)
+
+
+
+        do while(current_time < t/delta_t)
+            if(count >= cint) then
+                call write_checkpoint_file(datas,mu,ftot,coeffs,cpo,initial_conc,conc_std&
+                    ,nx,ny,m1,m2,k,bfe,Cint,t,delta_t,current_time,df_tol,random_seed,ncerr)
+                if(ncerr /= nf90_noerr) then
+                    print*, "There was an error writing the checkpoint file."
+                    print*, "Checkpoint test failed"
+                    stop
+                end if
+                count = 0
+            end if
+            count = count +1
+            current_time = current_time+1
+            datas(1,1,1) = datas(1,1,1)+1
+            mu(1,1) = mu(1,1)+2
+            ftot(1) = ftot(1)+3
+            if(current_time == 50023) then
+                exit
+            end if
+
+        end do
+
+        if(cpi /= "") then
+            call read_checkpoint_in(datas,mu,ftot, cpi,initial_conc,conc_std,coeffs,nx,&
+                ny,m1,m2,k,bfe,cint,cpo,t,delta_t,df_tol,current_time,random_seed,use_input,ncerr)
+            if(ncerr /= nf90_noerr) then
+                print*, "There was an error reading the checkpoint file."
+                print*, "Checkpoint test failed"
+                stop
+            end if
+        end if
+
+        if(current_time == 50000 .and. &
+            abs(mu(1,1)- 100001) < mu(1,1)/1e7 .and. &
+            abs(ftot(1)- 150001) < ftot(1)/1e7 .and. &
+            abs(datas(1,1,1)- 50001) < datas(1,1,1)/1e7 )then
+        else
+            print*, "Checkpoint test failed"
+            stop
+        end if
+
+        print*, "All checkpoint tests passed"
+    end subroutine checkpoint_test
+end module checkpoint_tester
---
+++ test/input_test.f90
@@ -0,0 +1,113 @@
+module input_tester
+    use input_params
+    use checkpointing
+    use netcdf
+    use iso_fortran_env
+
+    implicit none
+
+contains
+
+    subroutine input_test()
+
+
+        integer  :: nx,ny,cint,random_seed,err,i,count,ncerr,use_input,current_time
+        character(len=128) :: cpi,cpo
+        real(kind=REAL64) :: initial_conc, conc_std,m1,m2,k,bfe,t,delta_t,df_tol
+        real(kind=real64),dimension(:),allocatable :: coeffs
+        real(kind=real64), dimension(:,:,:), allocatable :: datas
+        real(kind=real64), dimension(:,:), allocatable :: mu
+        real(kind=real64), dimension(:), allocatable :: ftot
+
+
+
+        call read_params("input_test.txt",initial_conc,conc_std,coeffs,nx,&
+            ny,m1,m2,k,bfe,cint,cpi,cpo,t,delta_t,df_tol,random_seed,use_input,err)
+
+
+
+        if(err == -1) then
+            print*, "Input test 1 Failed"
+            stop
+        end if
+
+
+        if(abs(initial_conc - 0.7) < initial_conc/1e7 .and. &
+            abs(conc_std - 0.1) < conc_std/1e7 .and. &
+            nx == 50 .and. &
+            ny == 51 .and. &
+            abs(m1 - 1) < m1/1e7 .and. &
+            abs(m2 - 12) < m2/1e7 .and. &
+            abs(k - 1.6) < k/1e7 .and. &
+            abs(bfe - 1.7) < bfe/1e7 .and. &
+            cint == 50000 .and. &
+            cpo == "Out.cp" .and. &
+            size(coeffs) == 4 .and. &
+            abs(coeffs(1) - 2.1) < coeffs(1)/1e7 .and. &
+            abs(coeffs(2) - 2.2) < coeffs(2)/1e7 .and. &
+            abs(coeffs(3) - 2.3) < coeffs(3)/1e7 .and. &
+            abs(coeffs(4) - 2.4) < coeffs(4)/1e7 .and. &
+            abs(t - 1e-7) < t/1e7 .and. &
+            abs(delta_t - 1e-12) < delta_t/1e7 .and. &
+            abs(df_tol - 2) < df_tol/1e7 .and. &
+            Random_seed == 12345356 .and. &
+            Use_input == 0) then
+        else
+            print*, "Input test 1 Failed"
+            stop
+        end if
+
+
+
+        call read_params("input_test2.txt",initial_conc,conc_std,coeffs,nx,&
+            ny,m1,m2,k,bfe,cint,cpi,cpo,t,delta_t,df_tol,random_seed,use_input,err)
+
+
+        if(err == -1) then
+            print*, "Input test 2 Failed"
+            stop
+        end if
+
+        if(abs(initial_conc - 0.7) < initial_conc/1e7 .and. &
+            abs(conc_std - 0.1) < conc_std/1e7 .and. &
+            nx == 50 .and. &
+            ny == 51 .and. &
+            abs(m1 - 1) < m1/1e7 .and. &
+            abs(m2 - 12) < m2/1e7 .and. &
+            abs(k - 1.6) < k/1e7 .and. &
+            abs(bfe - 1.7) < bfe/1e7 .and. &
+            cint == 50000 .and. &
+            cpo == "Checkpoint.cpf" .and. &
+            cpi == "" .and. &
+            size(coeffs) == 5 .and. &
+            abs(coeffs(1) - 0) < 1/1e7 .and. &
+            abs(coeffs(2) - 0) < 1/1e7 .and. &
+            abs(coeffs(3) - 1.7) < abs(coeffs(3)/1e7) .and. &
+            abs(coeffs(4) - 0) < 1/1e7 .and. &
+            abs(coeffs(5)  + 1.7) < abs(coeffs(5)/1e7) .and. &
+            abs(t + 1) < abs(t/1e7) .and. &
+            abs(delta_t  + 1) < abs(delta_t/1e7) .and. &
+            abs(df_tol  + 1) < abs(df_tol/1e7) .and. &
+            Random_seed == -1 .and. &
+            Use_input == 0) then
+
+        else
+            print*, "Input test 2 Failed"
+            stop
+        end if
+
+
+        call read_params("input_test3.txt",initial_conc,conc_std,coeffs,nx,&
+            ny,m1,m2,k,bfe,cint,cpi,cpo,t,delta_t,df_tol,random_seed,use_input,err)
+
+        if(err /= -1) then
+            print*, "Input test 3 Failed"
+            stop
+        end if
+
+        print*, "All input test passed"
+
+    end subroutine input_test
+
+
+end module input_tester
