87805be75d9901fac4723e303ca615eec5c3c510
Dylan <Dylan.Morgan@warwick.ac.uk>
2022-05-16 13:02:39 +0100

    Created comms template #2

---
+++ src/comms.f90
@@ -0,0 +1,23 @@
+module comms
+
+    use mpi
+    use grid
+
+contains
+
+    subroutine comms_initialise()
+    end subroutine comms_initialise
+
+    subroutine comms_processor_map()
+    end subroutine comms_processor_map
+
+    subroutine comms_get_global_()
+    end subroutine comms_get_global_
+
+    subroutine comms_halo_swap()
+    end subroutine
+
+    subroutine comms_get_global_grid()
+    end subroutine
+
+end module comms
