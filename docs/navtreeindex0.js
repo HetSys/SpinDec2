43fc5a563c5f68e39ce95436713c4ce3886c77a4
Dylan <Dylan.Morgan@warwick.ac.uk>
2022-05-19 23:49:58 +0100

    Cleaned docs and made theme work

---
+++ docs/navtreeindex0.js
@@ -0,0 +1,15 @@
+var NAVTREEINDEX0 =
+{
+"cahn__hilliard_8f90_source.html":[0,0,0,0],
+"checkpoint_8f90_source.html":[0,0,0,1],
+"dir_68267d1309a1af8e8297ef4c3efbcdba.html":[0,0,0],
+"files.html":[0,0],
+"free__energy_8f90_source.html":[0,0,0,2],
+"grid_8f90_source.html":[0,0,0,3],
+"index.html":[],
+"input__parser_8f90_source.html":[0,0,0,4],
+"io_8f90_source.html":[0,0,0,5],
+"main_8f90_source.html":[0,0,0,6],
+"pages.html":[],
+"potentials_8f90_source.html":[0,0,0,7]
+};
